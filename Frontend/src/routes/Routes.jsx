33ca89d50a9655b40a03156971cb9fd50b2bafdb
jps27CSE <jackpritomsoren@hotmail.com>
2023-10-23 13:07:17 +0600

    firebase added

--- Frontend/src/routes/Routes.jsx
+++ Frontend/src/routes/Routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../components/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
